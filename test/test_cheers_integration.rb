aeddc8f2eaffc9f3dbc3be83747c766cb9d76837
Margaret Clinard <margaret.clinard@gmail.com>
2015-04-21 11:12:59 -0500

    Basic test set up.

---
+++ test/test_cheers_integration.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'minitest/autorun'
+
+class TestCheersIntegration < Minitest::Test
+
+  def test_that_the_tests_run
+    assert_equal true, false
+  end
+
+end
